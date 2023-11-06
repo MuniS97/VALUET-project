8a93983169acae8f59b12dd70ba561b68ce4830d
MuniS67 <tursunovmunis34@gmail.com>
2023-11-06 15:27:07 +0500

    added

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,37 @@
+import { getData } from "../../modules/helpers";
 import { eyeFunction } from "../../modules/ui";
 
+let form = document.forms.login;
 
+form.onsubmit = (e) => {
+  e.preventDefault();
 
+  let user = {};
+
+  let fm = new FormData(form);
+  fm.forEach((value, key) => {
+    user[key] = value;
+  });
+
+  getData("/users?email=" + user.email).then((res) => {
+    if (!res) return;
+    if (res.status !== 200 && res.status !== 201) return;
+    if (res.data.length === 0) {
+      alert("not found");
+      return;
+    }
+
+    let [res_user] = res.data;
+
+    if (+res_user.password === +user.password) {
+      delete res_user.password;
+      localStorage.setItem("user", JSON.stringify(res_user));
+      location.assign("/");
+    } else {
+      alert("wrong password");
+    }
+  });
+};
 
 let eye = document.querySelector("#img2");
 eye.onclick = () => {
