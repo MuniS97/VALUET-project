a118e24bea42b7335da6b91483b2c903ed9a28ee
MuniS67 <tursunovmunis34@gmail.com>
2023-11-06 13:13:45 +0500

    changes

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,1 +1,12 @@
-
+let eye = document.querySelector("#img2");
+eye.onclick = () => {
+  myFunction();
+};
+function myFunction() {
+  let x = document.querySelector("#inp");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
---
+++ pages/signUp/index.js
@@ -0,0 +1,13 @@
+let eye = document.querySelector("#img2");
+eye.onclick = () => {
+  myFunction();
+};
+
+function myFunction() {
+  let x = document.querySelector("#inp");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
