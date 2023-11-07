ffea86a9168a2b300201a30e43d6444c3e352473
MuniS67 <tursunovmunis34@gmail.com>
2023-11-07 14:41:55 +0500

    changed

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { headerCreate, dashboard_create } from "./modules/ui";
 import { user } from "./modules/user";
+import Chart from "chart.js/auto";
 
 headerCreate();
 dashboard_create(user);
@@ -11,3 +12,40 @@ currentDateText.innerHTML =
   new Date().getMonth() +
   " . " +
   new Date().getFullYear();
+
+let balanceBlock = document.querySelector("#balance1");
+
+const data = {
+  datasets: [
+    {
+      label: "My crypt",
+      data: [38, 15, 74],
+      backgroundColor: ["orange", "whitesmoke", "#1288e8"],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+let balance = new Chart(balanceBlock, {
+  type: "doughnut",
+  data: data,
+});
+
+let spendingBlock = document.querySelector("#spending2");
+
+const data2 = {
+  labels: [2, 4, 6, 8, 10, 12, 14, 16],
+  datasets: [
+    {
+      label: "Spending Activity",
+      data: [60, 50, 45, 75, 80, 90, 100],
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+    },
+  ],
+};
+let spending = new Chart(spendingBlock, {
+  type: "line",
+  data: data2,
+});
