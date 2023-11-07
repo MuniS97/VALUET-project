3296e016f4223b543b7adf4c68af74b7c3db0497
MuniS67 <tursunovmunis34@gmail.com>
2023-11-07 17:03:53 +0500

    change

--- main.js
+++ main.js
@@ -38,14 +38,43 @@ const data2 = {
   datasets: [
     {
       label: "Spending Activity",
-      data: [60, 50, 45, 75, 80, 90, 100],
+      data: [60, 50, 45, 80, 60, 70, 75, 100],
       fill: false,
       borderColor: "rgb(75, 192, 192)",
-      tension: 0.1,
+      tension: 0.5,
     },
   ],
 };
 let spending = new Chart(spendingBlock, {
   type: "line",
   data: data2,
+});
+
+let marketBlock = document.querySelector("#marketChart");
+const data3 = {
+  labels: [
+    "MDV 15",
+    "MDV 17",
+    "MDV 19",
+    "MDV 21",
+    "MDV 23",
+    "MDV 25",
+    "MDV 27",
+    "MDV 29",
+    "MDV 31",
+    "MDV 33",
+  ],
+  datasets: [
+    {
+      label: "Spending Activity",
+      data: [24000, 20000, 26000, 24000, 28000, 26000, 34000, 30000, 36000],
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.5,
+    },
+  ],
+};
+let market = new Chart(marketBlock, {
+  type: "line",
+  data: data3,
 });
